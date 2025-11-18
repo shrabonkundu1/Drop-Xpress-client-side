817b568a517bae8e944d051f48bede263392cfe0
Shrabon Kundu <shrabonkundu@gmail.com>
2025-11-19 01:42:49 +0600

    Implement a new navbar and footer

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import Navbar from '../page/shared/Navbar'
+import Footer from '../page/shared/Footer'
 
 const RootLayout = () => {
   return (
     <div>
+      <Navbar></Navbar>
         <Outlet></Outlet>
+        <Footer></Footer>
     </div>
   )
 }
